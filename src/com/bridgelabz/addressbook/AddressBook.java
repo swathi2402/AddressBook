9ddce4fe234318dfa7c22088b34070eadc81853d
Swathi <swathi.h@perfios.com>
2021-08-26 20:18:50 +0530

    DELETED: Unnecessary comments

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -7,11 +7,6 @@ public class AddressBook {
 		System.out.println("***** Welcome to Address Book Program *****");
 		
 		AllContacts addContact = new AllContacts();
-//		addContact.getAllContacts().add(new Contact("Swathi", "Hebbar", "Abcd Efgh",
-//				"Udupi", "Karnataka", "576224", "9900554433", "swathi.g@gmail.com"));
-//		System.out.print("Name: " + addContact.getAllContacts().get(0).getFirstName() + " ");
-//		System.out.println(addContact.getAllContacts().get(0).getLastName());
-//		System.out.println("Phone Number: " + addContact.getAllContacts().get(0).getPhoneNumber());
 		System.out.println("Enter details in the order First Name, Lsat Name, Address, City, State, "
 							+ "Pincode, Phone Number, Email Address");
 		
