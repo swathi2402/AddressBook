1fc5e160f890e32ef52de66d60e1682f5d7f9d50
Swathi <swathi.h@perfios.com>
2021-08-27 19:22:46 +0530

    DELETED: .java file and its references deleted to use array

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -3,11 +3,5 @@ package com.bridgelabz.addressbook;
 public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Address Book Program *****");
-		AllContacts addContact = new AllContacts();
-		addContact.getAllContacts().add(new Contact("Swathi", "Hebbar", "Abcd Efgh",
-				"Udupi", "Karnataka", "576224", "9900554433", "swathi.g@gmail.com"));
-		System.out.print("Name: " + addContact.getAllContacts().get(0).getFirstName() + " ");
-		System.out.println(addContact.getAllContacts().get(0).getLastName());
-		System.out.println("Phone Number: " + addContact.getAllContacts().get(0).getPhoneNumber());
 	}
 }
